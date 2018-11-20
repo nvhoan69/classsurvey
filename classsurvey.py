ce72c0dc8d9ab82704c8747c494ea488552b974c
vnhoan69 <boy98tx@gmail.com>
2018-11-20 17:24:55 +0700

    first commit

--- classsurvey.py
+++ classsurvey.py
@@ -5,10 +5,5 @@ from app import create_app, db
 app = create_app()
 Migrate(app, db)
 
-# from app.base.models import User, ACCESS
-# user = User(username='Admin', password='Admin', role=ACCESS['admin'])
-# db.session.add(user)
-# db.session.commit()
-
 if __name__ == '__main__':
     app.run(debug = True)
